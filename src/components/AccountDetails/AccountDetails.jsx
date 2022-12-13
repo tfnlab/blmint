2337f1a45ea6b9cbb837d780ff8d5f8443659851
tfnLAB <tfnlab@gmail.com>
2022-12-13 13:22:40 -0800

    update

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -4,13 +4,13 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
   return (
     <div>
       <div className="jumbotron">
-        <h1 className="display-5">SkellyClub</h1>
+        <h1 className="display-5">Burbank Live</h1>
         <div class="card col-md-12" >
           <div class="card-body">
             <p className="lead">
-                  Click the mint button above to  Mint a SkellyClub Token
+                  Click the mint button above to  Mint a Burbank Live Token
                   <hr className="my-4" />
-                  
+
             </p>
           </div>
         </div>
